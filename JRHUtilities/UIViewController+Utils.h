de57563397c9101f387fa6bb74ede13bcb859a5c
jayhickey <johnrhickey@gmail.com>
2015-02-09 18:49:24 -0500

    Added method to get top viewcontroller

---
+++ JRHUtilities/UIViewController+Utils.h
@@ -0,0 +1,15 @@
+//
+//  UIViewController+Utils.h
+//  JRHUtilities
+//
+//  Created by johnrhickey on 2/9/15.
+//  Copyright (c) 2015 Jay Hickey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (Utils)
+
++(UIViewController*) currentViewController;
+
+@end
