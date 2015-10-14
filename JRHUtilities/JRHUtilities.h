86ff0de30c24b756e1af58824b1906f400fe5c5f
jayhickey <johnrhickey@gmail.com>
2015-10-14 10:51:41 -0400

    Added umbrella header

---
+++ JRHUtilities/JRHUtilities.h
@@ -0,0 +1,23 @@
+//
+//  JRHUtilities.h
+//  JRHUtilities
+//
+//  Created by johnrhickey on 10/14/15.
+//  Copyright © 2015 Jay Hickey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+#import "NSArray+ArrayUtilities.h"
+#import "NSData+Base64.h"
+#import "NSDate+DateUtilities.h"
+#import "NSMutableArray+Randomize.h"
+#import "NSString+StringUtilities.h"
+#import "NSString+URLQuery.h"
+#import "UIColor+CustomColors.h"
+#import "UIFontDescriptor+AvenirNext.h"
+#import "UIImage+CreateImage.h"
+#import "UIImage+DrawOnImage.h"
+#import "UIImage+ImageEffects.h"
+#import "UIViewController+Utils.h"
